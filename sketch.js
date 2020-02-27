d7537ab3628e4dd6c8a81853da8dc28d28a1fc83
kelvin dizon <kidswoosh@icloud.com>
2020-02-26 22:31:05 -0800

    a

--- sketch.js
+++ sketch.js
@@ -1,17 +1,37 @@
 // dogs I have loved before
-let dogs = ["chase", "cyber"];
+let dogs = [{
+  name: "chase",
+  color: "dog treat color"
+}, {
+  name: "cyber",
+  color: "brown"
+}, {
+  name: "tony",
+  color: "black"
 
-function setup(){
-createCanvas(600, 600);
-background(200);
+}, {
+  name: "jm",
+  color: "white",
+}];
 
-console.log(dogs[0]);
+let randomIndex;
 
+function setup() {
+  createCanvas(600, 600);
+  background(200);
 
 }
 
+function draw() {
 
-function draw(){
+}
+
+function mousePrressed(){
+  background(random(200, 255));
+  randomIndex = int(random(dogs.length));
+  text(dogs[randomIndex].name, 50, 50);
+  dogs.splice(randomIndex, 1);
+
 
 
 
